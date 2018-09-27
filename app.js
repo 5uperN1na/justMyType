ad6eb9bf6efab10353dfe773aa07163bbc5dc6ff
Paula Suarez <pauladzuck@hotmail.com>
2018-09-27 18:16:59 -0500

    Adding updates to app.js file.

--- app.js
+++ app.js
@@ -5,22 +5,83 @@ $(document).ready(function () {
 
     $(document).keydown(function (e) {
         if (e.keyCode == 16) {
-            //testing when shift key is pressed
-            alert(e.which + " or Shift was pressed");
+            //testing if true
+            //alert(e.which + " or Shift was pressed");
 
             //show uppercase keyboard when shift key is pressed
             $('#keyboard-upper-container').show();
             //hide lowercase keyboard when shift key is pressed
             $('#keyboard-lower-container').hide();
+
+            $(document).keyup(function (e) {
+                $('#keyboard-lower-container').show();
+                $('#keyboard-upper-container').hide();
+            })
+        }
+
+        if (e.keyCode == 32) {
+            $('#32').css('background-color', 'yellow');
+        }
+
+        if (e.keyCode == 48) {
+
+            $('#48').css('background-color', 'yellow');
         }
         if (e.keyCode == 49) {
-            //testing when shift key is pressed
-            alert(e.which + " or 1 was pressed");
-            //$('#keyboard-lower-container span').text();
-            // $('#item1 span').html();
-            $("span:contains('49')").css('background-color', 'pink'); 
-            //$('keyboard-lower-container #49').css('background-color', 'pink');
+
+            $('#49').css('background-color', 'yellow');
         }
+
+        if (e.keyCode == 50) {
+
+            $('#50').css('background-color', 'yellow');
+        }
+
+        if (e.keyCode == 51) {
+
+            $('#51').css('background-color', 'yellow');
+        }
+
+        if (e.keyCode == 52) {
+
+            $('#52').css('background-color', 'yellow');
+        }
+
+        if (e.keyCode == 53) {
+
+            $('#53').css('background-color', 'yellow');
+        }
+
+        if (e.keyCode == 54) {
+
+            $('#54').css('background-color', 'yellow');
+        }
+
+        if (e.keyCode == 55) {
+
+            $('#55').css('background-color', 'yellow');
+        }
+
+        if (e.keyCode == 56) {
+
+            $('#56').css('background-color', 'yellow');
+        }
+
+        if (e.keyCode == 57) {
+
+            $('#57').css('background-color', 'yellow');
+        }
+
+        if (e.keyCode == 58) {
+
+            $('#58').css('background-color', 'yellow');
+        }
+
+        if (e.keyCode == 126) {
+
+            $('#126').css('background-color', 'yellow');
+        }
+
 
 
 
