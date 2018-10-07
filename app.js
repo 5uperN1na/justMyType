c040e90fc290a99bfa78c19e2921abd89732f975
Paula Suarez <pauladzuck@hotmail.com>
2018-10-07 16:53:43 -0500

    Adding more updates to app.js file

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ $(document).ready(function () {
 
         "ten ate neite ate nee enet ite ate inet ent eate",
         "Too ato too nOt enot one totA not anot tOO aNot",
-        // "oat itain oat tain nate eate tea anne inant nean",
-        // "itant eate anot eat nato inate eat anot tain eat",
-        // "nee ene ate ite tent tiet ent ine ene ete ene ate"
+        "oat itain oat tain nate eate tea anne inant nean",
+        "itant eate anot eat nato inate eat anot tain eat",
+        "nee ene ate ite tent tiet ent ine ene ete ene ate"
     ];
 
     //Global variables to for calcuations
